9fe208da926f094dce24e7ddeb133c41330b7789
christian <chrisjaccyn_10@hotmail.com>
2023-04-23 22:40:38 -0500

    Primer commit desde vs code try

--- src/app.js
+++ src/app.js
@@ -8,20 +8,46 @@ const port = 3000;
 
 const productManager = new ProductManager('./products.json');
 
+// app.get('/products', async (req, res) => {
+//   const limit = req.query.limit;
+//   const products = await productManager.getProducts();
+//   if (limit) {
+//     res.json(products.slice(0, limit));
+//   } else {
+//     res.json(products);
+//   }
+// });
 app.get('/products', async (req, res) => {
-  const limit = req.query.limit;
-  const products = await productManager.getProducts();
-  if (limit) {
-    res.json(products.slice(0, limit));
-  } else {
-    res.json(products);
+  try {
+    const limit = req.query.limit;
+    const products = await productManager.getProducts();
+    if (limit) {
+      res.json(products.slice(0, limit));
+    } else {
+      res.json(products);
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error  del Servidor' });
   }
 });
 
+
+// app.get('/products/:pid', async (req, res) => {
+//   const pid = req.params.pid;
+//   const product = await productManager.getProductById(pid);
+//   res.json(product);
+// });
+
 app.get('/products/:pid', async (req, res) => {
-  const pid = req.params.pid;
-  const product = await productManager.getProductById(pid);
-  res.json(product);
+  try {
+    const pid = req.params.pid;
+    const product = await productManager.getProductById(pid);
+    res.json(product);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error  del Servidor'});
+  }
 });
 
 app.listen(port, () => {
